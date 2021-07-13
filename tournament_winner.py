8442b87e894aaf1ddfa5ed2f271d2424f452542b
Anton <42787049+antonsoo@users.noreply.github.com>
2021-07-13 20:15:23 +0900

    Update tournament_winner.py

--- tournament_winner.py
+++ tournament_winner.py
@@ -4,20 +4,21 @@ def tournamentWinner(competitions, results):
     teams = {}
 	# populate the teams dictionary with losers and winners
 	# I added continue each time to speed up the code a little
+	# += 3 because we were told points increment by 3
 	for i in range(len(results)):
 		if results[i] == 1 and competitions[i][0] in teams: # home team won
-			teams[competitions[i][0]] += 1 
+			teams[competitions[i][0]] += 3 
 			continue 
 		if results[i] == 0 and competitions[i][1] in teams: # away-team won
-			teams[competitions[i][1]] += 1
+			teams[competitions[i][1]] += 3
 			continue
 		if results[i] == 1 and competitions[i][0] not in teams:  # home team won
-			teams[competitions[i][0]] = 1 # add the home-team that won
+			teams[competitions[i][0]] = 3 # add the home-team that won
 			teams[competitions[i][1]] = 0 # add the away-team that lost
 			continue
 		if results[i] == 0 and competitions[i][1] not in teams: # away-team won
 			teams[competitions[i][0]] = 0 # # add the home-team that lost
-			teams[competitions[i][1]] = 1 # add the away-team that won
+			teams[competitions[i][1]] = 3 # add the away-team that won
 			continue
 	
 	# find the maximum
