07e40e1b226494e6cc6f69d8c6f9bd4a37a3aff8
Anton <42787049+antonsoo@users.noreply.github.com>
2022-01-16 05:32:28 +0900

    Update moveElementToEnd.py

--- moveElementToEnd.py
+++ moveElementToEnd.py
@@ -4,14 +4,15 @@ def moveElementToEnd(array, toMove):
 	p2 = len(array) - 1
 	while p1 < p2: # exit when they meet or pass in the middle
 		# already in a good place:
-		if array[p2] == toMove:
+		if array[p2] == toMove: 
+			# can also make this into a while loop: i<j and array[p2] == toMove: p2 -=1
 			p2 -= 1
 			continue
 		# number at p1 is the right number:
 		elif array[p1] == toMove:
 			swap(p1, p2, array)
 			p1 += 1
-			p2 -= 1
+			#p2 -= 1 #unnecessary since above line does it
 		# numbers at p1 and p2 are both wrong, keep p2, change p1
 		else:
 			p1 += 1
