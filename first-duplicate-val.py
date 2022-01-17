6ad7041e64df291c91bf43cf13235e1b2ca674b0
Anton <42787049+antonsoo@users.noreply.github.com>
2022-01-17 20:50:12 +0900

    Update first-duplicate-val.py

--- first-duplicate-val.py
+++ first-duplicate-val.py
@@ -7,3 +7,18 @@ def firstDuplicateValue(array):
 		else: # seen it more than once
 			return val
 	return -1
+
+# O(n) time and O(1) space
+# do index=abs(val)-1 to find the index, -1 because array indeces start at 0.
+# the number at this index will tell you if you've seen a val like that before.
+# if you haven't seen it already, then turn the number at that index into a negative number.
+# we can do this because the input is pos integers (between 1 and n)
+def firstDuplicateValue(array):
+	for val in array: # go through vals in the array
+		index = abs(val) - 1
+		if array[index] < 0: # negative
+			return abs(val)
+		else:  # positive
+			# make it into a negative value that's already present there
+			array[index] *= -1
+	return -1 # nothing found
