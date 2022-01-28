bcae2e2dcf56aeca44b2e7b630fa4da1fb2391e6
Anton <42787049+antonsoo@users.noreply.github.com>
2022-01-28 20:19:15 +0900

    Update ways-to-make-change.py

--- dp/ways-to-make-change.py
+++ dp/ways-to-make-change.py
@@ -19,3 +19,29 @@ def numberOfWaysToMakeChange(n, denoms):
 				ways[amount] = ways[amount] + ways[amount - d]
 				# Ex: ways[10] = ways[10] + ways[10 - 5] = ways[10] + ways[5] = 2 + 2 = 4
 	return ways[-1] # if it doesn't change, like from o
+
+
+
+-------------------------my first try before watching the solution, not working:
+"""
+- problem: return the min number of coins from the given coin types (denoms) to get the target sum
+- solution: 
+make a coins array with the number of elements being the target sum + 1, this will hold the num of coins used for different sums
+dynamic programming with a base case being ...
+go through the array (target sum + 1) times, count up possible ways of doing the sums and store them
+return -1 at the end the algorithm doesn't find a target sum
+- complexity: O()
+"""
+def minNumberOfCoinsForChange(n, denoms):
+    coins = [0 for i in range(n + 1)] # starts with 0, we want to start from 1 so add len by 1
+	# base case 
+	#coins[1] = 0 # ??
+	
+	for i in range(n + 1):
+		for d in range(len(denoms)):
+			coins[i] += denoms[d] 
+			
+	for ind, coin in enumerate(coins):
+		if coin == n:
+			return ind
+	return -1 # else return -1
